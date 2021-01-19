a0e46df89176e1e6230c2efbafe08afc0bfdded2
Petka <petar.georgiev@gmail.com>
2021-01-19 13:57:22 +0200

    Initial commit.

---
+++ src/test/java/org/petka/pis/PisApplicationTests.java
@@ -0,0 +1,12 @@
+package org.petka.pis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
