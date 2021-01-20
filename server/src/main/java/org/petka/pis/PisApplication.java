d46a6ce87913eba148e3a633cf707403e8696ce5
Petka <petar.georgiev@gmail.com>
2021-01-20 18:29:21 +0200

    Added api-first to the project.

--- server/src/main/java/org/petka/pis/PisApplication.java
+++ server/src/main/java/org/petka/pis/PisApplication.java
@@ -2,12 +2,14 @@ package org.petka.pis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PisApplication {
 
     /**
      * Spring boot main class.
+     *
      * @param args application arguments.
      */
     public static void main(final String[] args) {
---
+++ server/src/main/java/org/petka/pis/controllers/TestController.java
@@ -0,0 +1,9 @@
+package org.petka.pis.controllers;
+
+import org.petka.pis.api.PetsApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController implements PetsApi {
+
+}
